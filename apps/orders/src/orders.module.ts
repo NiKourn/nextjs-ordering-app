f58e61ee0fcea9f3cf44ef4aefc070a95fe681f1
Nick Kourniotis <nick.kourniotis@nextpixel.com>
2024-06-27 17:04:33 +0100

    init commit

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { OrdersRepository } from './orders.repository';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+        // RMQ_URL: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService, OrdersRepository],
+})
+export class OrdersModule {}
